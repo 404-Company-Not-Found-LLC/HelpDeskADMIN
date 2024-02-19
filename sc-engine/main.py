8f736607bf2bb8aeb9b1ced228e6d8787beafa98
archways <107893165+archways404@users.noreply.github.com>
2024-02-19 21:09:50 +0100

    changes

--- sc-engine/main.py
+++ sc-engine/main.py
@@ -1,6 +1,11 @@
 import pytesseract
 from PIL import ImageGrab
 import pyautogui
+#import pytesseract 
+pytesseract.pytesseract.tesseract_cmd = r'C:\Program Files\Tesseract-OCR\tesseract.exe'
+
+import re
+
 
 # brew install tesseract
 
@@ -10,10 +15,10 @@ import pyautogui
 
 def capture_screenshot_and_extract_text():
     # Take a screenshot using PyAutoGUI
-    screenshot = pyautogui.screenshot()
+    #screenshot = pyautogui.screenshot()
 
     # Use PIL's ImageGrab (alternative way, if preferred)
-    # screenshot = ImageGrab.grab()
+    screenshot = ImageGrab.grab()
 
     # Save the screenshot for debugging purposes (optional)
     screenshot.save("screenshot.png")
@@ -23,6 +28,20 @@ def capture_screenshot_and_extract_text():
 
     # Print the extracted text
     print(extracted_text)
+
+    # Regular expression pattern to match the desired portion of the text
+    pattern = r"(API Azure Webhooks.*?)(?=Make invisi)"
+
+    # Using re.DOTALL to make the '.' special character match any character at all, including a newline
+    match = re.search(pattern, extracted_text, re.DOTALL)
+
+    if match:
+        desired_text = match.group(1)  # This should be your extracted text
+        print(desired_text)
+        with open("extracted_summary.txt", "w") as f:
+          f.write(desired_text)
+    else:
+        print("No match found")
 
     # Save the extracted text for debugging purposes (optional)
     with open("extracted_text.txt", "w") as f:
